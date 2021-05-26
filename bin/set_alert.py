9c72fb02d13751d11c50315000c3c26cbe2a43d9
Rishabh Karanth <me@karanth.dev>
2021-05-25 21:46:58 -0400

    Add arguments

---
+++ bin/set_alert.py
@@ -0,0 +1,12 @@
+import argparse
+
+# Define parser and arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--token", required=True)
+parser.add_argument("-t", "--term", required=True)
+parser.add_argument("-s", "--subreddit", required=True)
+parser.add_argument("-w", "--webhook", required=True)
+parser.add_argument("-f", "--frequency")
+
+args = parser.parse_args()
