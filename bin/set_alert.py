80cb12591fe3dc92f90b6b3ee16ec5e590722a5f
Rishabh Karanth <me@karanth.dev>
2021-07-13 23:29:43 -0400

    Handle reddit errors better

--- bin/set_alert.py
+++ bin/set_alert.py
@@ -2,6 +2,8 @@ import os
 import sys
 import argparse
 import re
+from time import sleep
+
 import requests
 import praw
 import prawcore
@@ -13,6 +15,8 @@ parser.add_argument("-r", "--regex", required=True, help="The regexes, separated
                                                          " titles")
 parser.add_argument("-s", "--subreddit", required=True, help="The subreddit to search.")
 parser.add_argument("-w", "--webhook", required=True, help="The Discord webhook to send messages to.")
+parser.add_argument("--retries", default=5, help="Number of times to retry upon a failed connection to reddit. Defaults"
+                                                 " to 5.")
 
 args = parser.parse_args()
 
@@ -46,12 +50,30 @@ if not requests.post(args.webhook, {"content": "Relert is starting...", "usernam
 subreddit_con = reddit_con.subreddit(args.subreddit)
 
 # Start listening to the stream and let us know if something shows up.
-for submission in subreddit_con.stream.submissions():
-    # If the submission matches our term, let's send an alert!
-    for regex in str(args.regex).split(','):
-        if re.search(regex, submission.title) is not None:
-            print("Submission title \"%s\" matches specified regex \"%s\". Sending to discord..." % (submission.title, regex))
-            message = "-----\nRELERT: Regex *\"%s\"* match found in Subreddit *\"r/%s\"*:\n\n" \
-                      "**Submission Title:** %s\n**Link:** %s" % \
-                      (regex, args.subreddit, submission.title, submission.url)
-            requests.post(args.webhook, {"content": message, "username": "r/"+args.subreddit})
+num_retries = 0
+wait_seconds = 15
+while True:
+    try:
+        for submission in subreddit_con.stream.submissions():
+            num_retries = 0
+            wait_seconds = 15
+            # If the submission matches our term, let's send an alert!
+            for regex in str(args.regex).split(','):
+                if re.search(regex, submission.title) is not None:
+                    print("Submission title \"%s\" matches specified regex \"%s\". Sending to discord..." %
+                          (submission.title, regex))
+                    message = "-----\nRELERT: Regex *\"%s\"* match found in Subreddit *\"r/%s\"*:\n\n" \
+                              "**Submission Title:** %s\n**Link:** %s" % \
+                              (regex, args.subreddit, submission.title, submission.url)
+                    requests.post(args.webhook, {"content": message, "username": "r/"+args.subreddit})
+    except prawcore.exceptions.ServerError as e:
+        num_retries += 1
+        print(e)
+        if num_retries > args.retries:
+            print("Max number of retries reached, shutting down for now.")
+            sys.exit(1)
+        else:
+            print("Sleeping for %d seconds. I will make %d more attempts before shutting down." %
+                  (wait_seconds, args.retries - num_retries))
+            wait_seconds *= 2
+            sleep(wait_seconds)
